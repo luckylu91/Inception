a3154edd56d46093d5967f737e1f4fb2f6d508dd
luckylu91 <zins.lucas@gmail.com>
2021-08-08 09:26:52 +0200

    push

---
+++ srcs/requirements/db/scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+service mysql start
+
+commands=$(cat << END_OF_COMMANDS
+CREATE USER '$MYSQL_USER';
+CREATE DATABASE $WORDPRESS_DATABASE;
+GRANT ALL PRIVILEGES on $WORDPRESS_DATABASE.* TO '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+END_OF_COMMANDS
+)
+
+echo "===MySQL commands==="
+echo "$commands"
+echo "$commands" | mysql -u root -password="$MYSQL_ROOT_PASSWORD"
+echo "===End MySQL commands==="
+
+#  "GRANT ALL PRIVILEGES on $WORDPRESS_DATABASE.* TO '$MYSQL_USER'@'wordpress' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root -p
